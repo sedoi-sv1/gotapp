c61b33f3a2684d517a979943eab1fdd9fb1cdcab
Sedoi <sedoi.sv1@yandex.ru>
2022-04-16 05:22:47 +0700

    Урок 2.5. Паттерны React

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
 import './app.css';
+import ErrorMessange from '../errorMessange';
+import CharacterPage from '../characterPage';
 
 
 /*function RandomCha(props) {
@@ -28,8 +28,16 @@ export default class App extends Component {
     state = {
         showRandomChar: true,
         /* состояние показывающее выбраного персонажа */
-        selectedChar: 101
+        
+        error: false
     };
+
+    componentDidCatch() {
+        console.log('error');
+        this.setState({
+            error: true
+        })
+    }
     
     /*charToggleClick() {
         this.setState(state => ({
@@ -45,14 +53,15 @@ export default class App extends Component {
         });
     };
 
-    /* Создаём обработчик событий который будет устанавливать выбранный id в selectedChar */
-    onCharSelected = (id) => {
-        this.setState({selectedChar: id
-        })
-    }
+
     
 render() {
     const char = this.state.showRandomChar ? <RandomChar/> : null;
+
+    if (this.state.error) {
+        return <ErrorMessange/>
+    }
+
     return (
         <> 
             <Container>
@@ -74,20 +83,14 @@ render() {
                     </button> 
                     
                 </Container>*/}
-                
-                <Row>
-                    <Col md='6'>
-                        {/* Берём id передаём на уровень выше в app.js */}
-                        <ItemList onCharSelected={this.onCharSelected}/>
-                    </Col>
-                    <Col md='6'>
-                        {/* Передаём выбранный id в CharDetails  */}
-                        <CharDetails charId={this.state.selectedChar}/>
-                    </Col>
-                </Row>
+
+                <CharacterPage/>
+                <CharacterPage/>
+                <CharacterPage/>
+            
             </Container>
         </>
-    );
+    )
 }        
     
-}
+};
---
+++ src/components/characterPage/characterPage.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Col, Row, Container} from 'reactstrap';
+import ItemList from '../itemList';
+import CharDetails from '../charDetails';
+import ErrorMessange from '../errorMessange'
+
+export default class CharacterPage extends Component {
+
+    state  = {
+        selectedChar: 130,
+        error: false
+    }
+
+     /* Создаём обработчик событий который будет устанавливать выбранный id в selectedChar */
+    onCharSelected = (id) => {
+        this.setState({selectedChar: id
+        })
+    }
+
+    componentDidCatch() {
+        this.setState({
+            error: true
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessange/>
+        }
+        return(
+            <Row>
+            <Col md='6'>
+                {/* Берём id передаём на уровень выше в app.js */}
+                <ItemList onCharSelected={this.onCharSelected}/>
+            </Col>
+            <Col md='6'>
+                {/* Передаём выбранный id в CharDetails  */}
+                <CharDetails charId={this.state.selectedChar}/>
+            </Col>
+        </Row>
+        )
+    }
+}
